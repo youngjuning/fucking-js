ef1336e552cefc88a830a1858ebddc6ea20e3dec
紫竹 <luozhu2021@gmail.com>
2025-02-27 09:00:07 +0800

    chore: ListNode 添加类型声明

--- src/LinkedList/ListNode.ts
+++ src/LinkedList/ListNode.ts
@@ -1,5 +1,7 @@
-export class ListNode {
-  constructor(element) {
+export class ListNode<T> {
+  element: T;
+  next?: ListNode<T>;
+  constructor(element: T) {
     // 数据域，存储节点的值
     this.element = element;
     // 指针域，指向下一个节点
